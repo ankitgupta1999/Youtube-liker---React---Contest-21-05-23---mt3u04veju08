603b200cb4a4edd2ec64327557040cb285c2fc37
ankitgupta1999 <120298083+ankitgupta1999@users.noreply.github.com>
2023-05-21 19:33:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,16 @@ import '../styles/App.css';
 import like from '../like.svg';
 
 const App = () => {
-  const [count,setCount] = React.useState();
-  
+  const [count,setCount] = React.useState(0);
+  const handleclick=()=>{
+    setCount(count+1);
+    
+  }
   return (
     <div id="main">
-      <img id="like-btn-img" style={{fill:'white',width:'100px',backgroundColor:'rgba(255,0,0,0)'}} />
-      <h3>Likes: <span id="like-counter">0</span></h3>
+      <img id="like-btn-img" src='../like.svg' onClick={handleclick}
+       style={{fill:'white',width:'100px',backgroundColor:`rgba(255,0,0,${count/10})`}} />
+      <h3>Likes: <span id="like-counter">{count}</span></h3>
     </div>
   )
 }
